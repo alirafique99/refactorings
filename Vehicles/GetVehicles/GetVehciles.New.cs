7d4e077d50580366192cc3c6c9a7deb0e86351cd
Muhammad Ali Rafique <mali@icaremanager.com>
2023-09-08 03:07:02 -0700

    Update GetVehciles.New.cs

--- Vehicles/GetVehicles/GetVehciles.New.cs
+++ Vehicles/GetVehicles/GetVehciles.New.cs
@@ -1,16 +1,19 @@
-public async Task<List<VehicleBasicInfoDto>> GetVehicles(VehicleFilters filters)
-{
-    filters.FacilityId = _loggedInUser.AlfId;
-    List<int?> assignedVehicles = new List<int?>();
-
-    if (ShouldBeFilteredByAssignments(filters))
+public class Vehicle{
+    public async Task<List<VehicleBasicInfoDto>> GetVehicles(VehicleFilters filters)
     {
-        assignedVehicles = await FilterVehiclesByAssignments(filters, new List<int>());
-        if (assignedVehicles.isValidList()) filters.VehicleIds = assignedVehicles;
+        filters.FacilityId = _loggedInUser.AlfId;
+        List<int?> assignedVehicles = new List<int?>();
+    
+        if (ShouldBeFilteredByAssignments(filters))
+        {
+            assignedVehicles = await FilterVehiclesByAssignments(filters, new List<int>());
+            if (assignedVehicles.isValidList()) filters.VehicleIds = assignedVehicles;
+        }
+    
+        var machineMakes = await GetMachineMakes(filters);
+        var vehicles = await GetVehiclesWithSpec(filters, machineMakes);
+    
+        return vehicles;
     }
-
-    var machineMakes = await GetMachineMakes(filters);
-    var vehicles = await GetVehiclesWithSpec(filters, machineMakes);
-
-    return vehicles;
+    
 }
